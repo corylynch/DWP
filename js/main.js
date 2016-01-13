c3030bc27816dcc9b949e400e851b6c873afea89
Cory Lynch <corylynchsrq@gmail.com>
2016-01-13 14:18:55 -0500

    Added full HTML for index.html

---
+++ js/main.js
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+<head>
+	<meta charset = 'utf-8'>
+	<title>Cory Lynch</title>
+	<link rel = 'stylesheet' href = 'css/styles.css'>
+
+
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+	<link rel="icon" type="image/ico" href="images/favicon.ico"/>
+</head>
+<body>
+	<div id = 'nav'>
+		<div class = 'container'>
+			<ul>
+				<li><a href = '#about-me' id = 'about-me-button'>About me</a></li>
+				<li><a href = '#portfolio' id = 'portfolio-button'>My work</a></li>
+				<li><a href = '#contact' id = 'contact-button'>Contact</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class = 'big-container' id = 'intro'>
+		<div class = 'container'>
+			<!-- <img src = 'images/bulb.png' id = 'bulb'> -->
+			<h1>Hi, I'm Cory. <br><br><br> I make <b>nifty</b> websites.</h1>
+		</div>
+	</div>
+	<div class = 'big-container one' id = 'about-me'>
+		<div class = 'container'>
+			<img src = 'images/me.png' id = 'me'>
+			<div id = 'my-info'>
+				<h1 id = 'name'>Cory Lynch</h1>
+				<p id = 'location'><img src = 'images/location.png' id = 'location-icon'> Orlando, FL</p>
+				<p id = 'description'>I am a full time student at Full Sail university currently studying Web Design & Development. I have a passion for web and <b>an eye for great design.</b></p>
+			</div>
+			<div id = 'skills'>
+				<p class = 'one'>HTML5</p>
+				<p class = 'one'>CSS3</p>
+				<p class = 'one'>jQuery</p>
+				<p class = 'two'>Github</p>
+				<p class = 'two'>Adobe Illustrator</p>
+				<p class = 'two'>Adobe Photoshop</p>
+			</div>
+		</div>
+	</div>
+	<div class = 'big-container' id = 'portfolio'>
+		<div class = 'container'>
+			
+		</div>
+	</div>
+	<div class = 'big-container' id = 'contact'>
+		<div class = 'container'>
+			<h1>Let's talk.</h1>
+			<script type="text/javascript" src="http://form.jotform.us/jsform/40848720855159"></script>
+		</div>
+	</div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src = 'js/main.js' type = 'text/javascript'></script>
+<script src = 'js/jquery-1.10.2.js' type = 'text/javascript'></script>
+<script src = 'js/jquery-ui-1.10.4.custom.js' type = 'text/javascript'></script>
+<script src = 'js/jquery-ui-1.10.4.custom.minjs' type = 'text/javascript'></script>
+</body>
+</html>
